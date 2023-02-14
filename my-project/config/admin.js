d9c76e18b5690fca5993d12a99ce78ca6be52198
climax-coder <seniorprodev@gmail.com>
2023-02-14 17:09:20 +0300

    setup strapi

--- my-project/config/admin.js
+++ my-project/config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "U31fpYyKI9p/c67CYedDbA=="),
+  },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '341d2f233a19dc6ebcd87f1f0a8878bf'),
+    secret: env("ADMIN_JWT_SECRET", "341d2f233a19dc6ebcd87f1f0a8878bf"),
   },
 });
